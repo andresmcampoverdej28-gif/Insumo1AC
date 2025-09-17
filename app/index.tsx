a780314f61ca6bd5b6b22c479a97649e197a221f
andresm.campoverdej.28 <andresm.campoverdej.28@gmail.com>
2025-09-17 19:24:43 +0000

    ✨feat(app): Instalar NativeWind en la aplicación. - Editar los archivos metro.config y babel.config - (index) Insertar estilo de prueba en index - Creamos el archivo global.css - Instalamos dependencias para NativeWind - Creamos Tailwind.config con el comando 'npx tailwindcss init'

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,13 @@
+import "@/global.css";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View className="flex-col bg-white w-screen-safe h-screen-safe">
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
